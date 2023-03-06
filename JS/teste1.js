fb6706ea3537aac77e1d77edd4a7da0fa6f4db6c
Yêda Silva <yedagabriela@live.com>
2023-03-06 16:38:58 -0300

    Modificações JS e HTML
    Modificação na ação dos botões.
    
    Modificação  rótulo do botão

--- JS/teste1.js
+++ JS/teste1.js
@@ -4,15 +4,12 @@
   var CopyButton = document.getElementById("copyButton");
   var ClearButton = document.getElementById("clearButton");
   
-
-  CopyButton.addEventListener("click", Copy);
-  
+ 
   function Copy(){
     TextArea.value = InputBox.value;    
     }
 
-  ClearButton.addEventListener("click",Clear);
-  
+    
   function Clear(){
     InputBox.value = "";
     TextArea.value = "";    
