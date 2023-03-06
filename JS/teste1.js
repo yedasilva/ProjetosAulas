8f1e7c7619976b68140f8e29abb5c59ab8da9be7
Yêda Silva <yedagabriela@live.com>
2023-03-06 17:00:21 -0300

    Alteracoes para treino

--- JS/teste1.js
+++ JS/teste1.js
@@ -1,17 +1,2 @@
   
-  var InputBox = document.getElementById("inputBox");
-  var TextArea = document.getElementById("textArea");
-  var CopyButton = document.getElementById("copyButton");
-  var ClearButton = document.getElementById("clearButton");
-  
  
-  function Copy(){
-    TextArea.value = InputBox.value;    
-    }
-
-    
-  function Clear(){
-    InputBox.value = "";
-    TextArea.value = "";    
-    }
-
